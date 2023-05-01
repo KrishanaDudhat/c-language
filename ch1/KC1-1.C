2d4a7f7f6499f8422dcdaa41350b2bd389c18cda
KrishanaDudhat <krishanadudhat2007@gmail.com>
2023-05-01 16:53:31 +0530

    ch1-intro of c language

---
+++ ch1/KC1-1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+clrscr();
+printf("Name\t\t:krishana dudhat");
+printf("\nAge\t\t:16");
+printf("\nCours\t\t:c language");
+printf("\nSchool\t\t:P.P.Savani");
+printf("\nHobby\t\t:wollyball,traveling");
+printf("\nMo numbe\t:+81 231654987");
+printf("\nLiving city\t:surat");
+printf("\nEducation\t:10th pass");
+getch();
+}
---
+++ ch1/KC1-2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+clrscr();
+printf("--------\n");
+printf("|      |\n");
+printf("K      |\n");
+printf("R      |\n");
+printf("I      |\n");
+printf("S      |\n");
+printf("H      |\n");
+printf("A      |\n");
+printf("N      |\n");
+printf("A      |\n");
+printf("|      |\n");
+printf("--------\n");
+getch();
+}
