fcab74f2be057c3671bf80dec3a5f78500952555
KrishanaDudhat <krishanadudhat2007@gmail.com>
2023-05-02 16:47:49 +0530

    operators 4.1

---
+++ ch4/KC-4-2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+    int a,b,c;
+    clrscr();
+    printf("enter a=");
+    scanf("%d",&a);
+    printf("enter b=");
+    scanf("%d",&b);
+    printf("a=%d b=%d",b,a);
+    getch();
+}
---
+++ ch4/KC-4-4.C
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+    int x,y;
+    clrscr();
+    printf("enetr x=");
+    scanf("%d",&x);
+    printf("enetr y=");
+    scanf("%d",&y);
+    printf("(x-Y)^2 :%d",x*x-y*y-2*x*y);
+    getch();
+    }
